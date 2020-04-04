c085c8323b9d920271fd5ff8cc67d6e794a721b5
Lucas Gabriel <lucasgabrielsampaio2019@gmail.com>
2020-04-04 20:39:30 -0300

    creation of the registration page and stylization of the same

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://kit.fontawesome.com/f3bfbc9f38.js" crossorigin="anonymous"></script>
   <link href="<?php echo MAIN_PATH; ?>css/style.css" rel="stylesheet" />
   <link href="<?php echo FONT; ?>" rel="stylesheet">
   <title>Stock System</title>
 </head>
 <body>
     <?php include_once('pages/header.php'); ?>
-    <?php Stock::loadPage(); ?>
+    <div class="cont">
+        <?php Stock::loadPage(); ?>
+    </div><!--cont-->
 </body>
 </html>
--- pages/includes/register.php
+++ pages/includes/register.php
@@ -1,1 +1,42 @@
-<h2>register stock</h2>
+<div class="container-register">
+    <div class="box-form">
+      <h2><i class="fas fa-box"></i> Register Product</h2>
+          <div class="box-txt">
+        </div><!--box-txt-->
+        <div class="container">
+            <form method="post" enctype="multipart/form-data">
+                <div class="form-g">
+                    <label>Product's name</label>
+                    <input type="text" name="name">
+                </div><!--form-g-->
+
+                <div class="form-g">
+                    <label>Description</label>
+                    <input type="text" name="description">
+                </div><!--form-g-->
+
+                <div class="form-g">
+                    <label>Provider</label>
+                    <input type="text" name="provider">
+                </div><!--form-g-->
+
+                <div class="form-g">
+                    <label>Manufacturer</label>
+                    <input type="text" name="manufacturer">
+                </div><!--form-g-->
+
+                <div class="form-g">
+                    <label>Amount</label>
+                    <input type="number" name="amount">
+                </div><!--form-g-->
+
+                <div class="form-g">
+                    <label>Image</label>
+                    <input type="file" name="image">
+                </div><!--form-g-->
+
+                <input type="submit" name="action" value="Register!">
+            </form>
+        </div><!--container-->
+    </div><!--box-form-->
+</div><!--container-->
